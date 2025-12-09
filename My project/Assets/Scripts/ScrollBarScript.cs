199c660123a10c2f34f676ccef7c94680e6ab7c2
azureeal <ttaaddi@gaming.tech>
2025-12-09 10:07:56 +0100

    .

--- My project/Assets/Scripts/ScrollBarScript.cs
+++ My project/Assets/Scripts/ScrollBarScript.cs
@@ -3,21 +3,18 @@ using UnityEngine.UI;
 
 public class ScrollBarScript : MonoBehaviour
 {
-    [SerializeField] RectTransform content;   // The object with the VerticalLayoutGroup
-    [SerializeField] float scrollRange = 500; // How far content can move (adjust this!)
+    [SerializeField] RectTransform content;
+    [SerializeField] float scrollRange = 500;
 
-    private Scrollbar bar;
+    [SerializeField] private Scrollbar bar;
 
     void Start()
     {
-        bar = GetComponent<Scrollbar>();
     }
 
     void Update()
     {
-        // map scrollbar.value (0 to 1) to content position
         float y = Mathf.Lerp(0, scrollRange, bar.value);
-
         content.anchoredPosition = new Vector2(content.anchoredPosition.x, y);
     }
 }
